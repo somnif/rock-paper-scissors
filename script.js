1b5619280004ed8e308e46242f71ce855a8bc142
somnif <teewaard@gmail.com>
2024-05-10 19:52:56 +1200

    Add jquery, and added HTML.

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerDecision () {
+    switch (Math.floor(Math.random()*3)){
+        case (0):
+            return "rock";
+            break;
+        case (1):
+            return "paper";
+            break;
+        case (2):
+            return "scissor"
+            break
+    }
+}
